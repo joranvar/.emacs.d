ed34748870dd2ad44c04be73052c4644c457b902
joranvar <bart.post@gmail.com>
2015-01-02 16:28:18 +0100

    Added solarized-light as safe theme

--- custom.el
+++ custom.el
@@ -6,8 +6,14 @@
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(flycheck-csharp-omnisharp-curl-executable "curl")
  '(haskell-font-lock-symbols (quote unicode))
  '(helm-github-stars-username "joranvar")
  '(omnisharp-imenu-support t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
