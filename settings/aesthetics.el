ee9706aaa67afe252004d13517c9a2b3a5a5738b
joranvar <bart.post@gmail.com>
2015-01-03 19:26:40 +0100

    Smaller default font

--- settings/aesthetics.el
+++ settings/aesthetics.el
@@ -8,5 +8,8 @@
 (require-package 'color-theme)
 (require-package 'color-theme-solarized)
 
+;; I like to see more, smaller is better
+(set-face-attribute 'default nil :height 100)
+
 (provide 'aesthetics)
 ;;; aesthetics.el ends here
