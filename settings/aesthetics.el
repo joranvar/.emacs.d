144905725b44db87aa4539d6423043f9f1b4dd73
joranvar <bart.post@gmail.com>
2015-01-11 20:10:38 +0100

    Toggle between light and dark solarized theme

--- settings/aesthetics.el
+++ settings/aesthetics.el
@@ -8,6 +8,9 @@
 (require-package 'color-theme)
 (require-package 'color-theme-solarized)
 
+;; Toggle solarized light/dark
+(global-set-key (kbd "C-c t") (lambda () (interactive) (progn (setq frame-background-mode (if (eq frame-background-mode 'light) 'dark 'light)) (load-theme 'solarized))))
+
 ;; I like to see more, smaller is better
 (set-face-attribute 'default nil :height 100)
 
